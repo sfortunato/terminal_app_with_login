487fa9784c4bfb436a62d4d242b32b22e009543d
Susanne Fortunato <susannefortunato@Susannes-MacBook.local>
2018-03-22 08:03:32 -0400

    added user_access and user.py, removed some dependencies - need to remove more from run_app

---
+++ user.py
@@ -0,0 +1,44 @@
+import cPickle as pickle 
+import os
+
+
+class User:
+	def __init__(self, uid, username, email, password):
+		"""initializes new user, should only be called via user_access.create() which will generate the uid"""
+		self.uid = uid
+		self.username = username
+		self.email = email
+		self.password = password
+		self.first_name = "not initiated"
+		self.middle_name = "not initiated"
+		self.last_name = "not initiated"
+		self.dob = "not initiated"
+
+	def printAll(self):
+		"""prints all user data, abstracts sensitive information"""
+		print "uid          ", self.uid
+		print "username:    ", self.username
+		print "email:       ", self.email
+		print "password:    ", "#######"
+		print "first name:  ", self.first_name
+		print "middle name: ", self.middle_name
+		print "last name:   ", self.last_name
+		print "DOB:         ", self.dob
+		print ""
+
+	def persistToFile(self):
+		"""soft creates / finds user's user_data folder, persists using pickle"""
+		path_to_uid_dir = "user_data/" + self.uid
+		os.system("mkdir -p " + path_to_uid_dir)
+		user_file = path_to_uid_dir + "/" + "user_data.file"
+		with open(user_file, "wb") as f:
+				pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
+
+	@staticmethod 		
+	def loadFromFile(uid):
+		"""finds a user's user_data folder, loads using pickle, returns user object"""
+		path_to_uid_dir = "user_data/" + uid
+		user_file = path_to_uid_dir + "/" + "user_data.file"
+		with open(user_file, "rb") as f:
+			user = pickle.load(f)
+		return user
---
+++ user_access.py
@@ -0,0 +1,77 @@
+import cPickle as pickle
+import os
+
+from user import User
+
+
+class UserAccess:
+	def __init__(self):
+		self.n = ""
+
+	def assignNewUID(self):
+		"""finds highest known UID, assigns a higher one, maintains preceding 0s and can set first UID if none exist"""
+		d = './user_data/'
+		os.system("mkdir -p " + d)
+		uids_exist = [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
+		if len(uids_exist) == 0:
+			return "000000001"
+		uids_exist = [o.split("user_data/")[1] for o in uids_exist]
+		print uids_exist
+		uids_exist = sorted(uids_exist)
+		print uids_exist
+		highest_known_uid = uids_exist[-1]
+		new_uid = int(highest_known_uid) + 1
+		new_uid = str(new_uid).zfill(9) #turns into string, fills in leading zeros
+		print new_uid
+		return new_uid
+
+	def create(self, username, email, password):
+		"""checks if username or email exist for another account, if does, returns None; else creates user data folder, assigns a UID, and saves data to folder"""
+		d = './user_data/'
+		os.system("mkdir -p " + d)
+		uids_exist = [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
+		for i in uids_exist:
+			user_file = i + "/" + "user_data.file"
+			with open(user_file, "rb") as f:
+				user = pickle.load(f)
+			if user.username == username:
+				return None
+			if user.email == email:
+				return None
+		#if doesn't, create a user & return user object
+		new_uid = self.assignNewUID()
+		new_user = User(new_uid, username, email, password)
+		new_user.persistToFile()
+		new_user.printAll()
+		return new_user
+
+
+	def login(self, username, password):
+		"""finds existing user and returns user object, if password incorrect it returns None"""
+		d = './user_data/'
+		os.system("mkdir -p " + d)
+		uids_exist = [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
+		for i in uids_exist:
+			user_file = i + "/" + "user_data.file"
+			with open(user_file, "rb") as f:
+				user = pickle.load(f)
+			if user.username == username:
+				if user.password == password:
+					user.printAll()
+					return user
+				else:
+					return None
+
+
+	def printAll(self, user):
+		"""prints all user data"""
+		user.printAll()
+
+
+	def persistToFile(self, user):
+		"""saves user object to file via User class"""
+		user.persistToFile()
+
+
+
+
